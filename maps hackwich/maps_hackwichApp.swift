de555f5ef23138094196bf117289107ddfd9e75d
sol gudmin <115568958+schnappiiiiiiii@users.noreply.github.com>
2023-02-08 09:40:41 -0600

    Initial Commit

---
+++ maps hackwich/maps_hackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  maps_hackwichApp.swift
+//  maps hackwich
+//
+//  Created by caden christesen on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct maps_hackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
